b73ad03892652baa1a0e9b80f5d0275110356ffe
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-15 15:17:52 +0300

    displaying data from an API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GithubUser({ name, company, location, avatar }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <h2>{company}</h2>
+      <p>{location}</p>
+      <img src={avatar} alt="user avatar" />
+    </>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -9,7 +20,15 @@ function App() {
       .then((data) => setData(data)); // or simply use shorthand.then(setData)
   }, []);
 
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (data)
+    return (
+      <GithubUser
+        name={data.name}
+        company={data.company}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
+    );
 
   return <h1>Data</h1>;
 }
