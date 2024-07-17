e5426e9222962befb106e78da15d5d429b901541
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-17 17:06:07 +0300

    handle loading states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-/* import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import GithubUser from "./GithubUser";
 
-function GithubUser({ name, company, location, avatar }) {
-  return (
-    <>
-      <h1>{name}</h1>
-      <h2>{company}</h2>
-      <p>{location}</p>
-      <img src={avatar} alt="user avatar" />
-    </>
-  );
-}
-
+// Displaying data from an API
+/*
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -29,47 +20,28 @@ function App() {
         avatar={data.avatar_url}
       />
     );
-
-  return <h1>Data</h1>;
 }
-
-export default App;
 */
 
-import React, { useEffect, useState } from "react";
-function GithubUser({ name, company, location, avatar }) {
-  return (
-    <>
-      <h1>{name}</h1>
-      <h2>{company}</h2>
-      <p>{location}</p>
-      <img src={avatar} alt={name} height={120} />
-    </>
-  );
-}
+// Handling Loading States
 const App = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   useEffect(() => {
     setLoading(true);
-    fetch("https://api.github.com/users/o-brayarn")
-      .then((res) => res.json)
-      .then((data) => setData(data))
+    fetch(`https://api.github.com/users/o-brayarn`)
+      .then((res) => res.json())
+      .then(setData)
       .then(() => setLoading(false))
       .catch(setError);
   }, []);
-
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <h1>Page loading . . .</h1>;
   if (error) return <pre>{JSON.stringify(error)}</pre>;
   if (!data) return null;
+
   return (
-    <GithubUser
-      name={data.name}
-      company={data.company}
-      location={data.location}
-      avatar={data.avatar_url}
-    />
+    <GithubUser name={data.name} blog={data.blog} avatar={data.avatar_url} />
   );
 };
 
